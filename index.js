85434f3af80feb8cecad6d29b5965e5635c068ee
Chien Tran <70304839+chientrm@users.noreply.github.com>
2022-10-07 07:10:27 +0700

    fix

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Test");
+import express from "express";
+
+const port = proces.env.PORT,
+  app = express()
+    .get("/health", (_, res) => {
+      res.send("OK");
+    })
+    .listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
