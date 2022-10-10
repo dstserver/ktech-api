a5111f0a81a3d06d753be51164e841e1013d878b
Chien Tran <70304839+chientrm@users.noreply.github.com>
2022-10-11 05:06:36 +0700

    f

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express"),
+  multer = require("multer"),
+  upload = multer({ dest: "uploads/" }),
   port = process.env.PORT,
   server = express()
     .get("/health", (_, res) => {
       res.send("OK");
+    })
+    .post("png2tex", upload.single("file"), (req, res, next) => {
+      console.log(req.file, req.body);
     })
     .listen(port, () => {
       console.log(`listening on port ${port}`);
